81b6c33b990a74862c6db22d239babbf98dd1ca5
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-29 16:25:08 -0500

    Finished Workshop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,59 @@ package com.company;
 
 public class Main {
 
+    private String memberVariable = "I am a member Variable, I have class level scope. ";
+    private int memberInt = 10;
+    private int count = 0;
+
     public static void main(String[] args) {
-	// write your code here
+
+        Main main = new Main();
+        main.aMethod();
+        main.anotherMethod();
+        main.methodForLoopVariables();
     }
+    private void aMethod() {
+        String localVariable = "I am a local variable, I can only be used in this method. ";
+
+        System.out.print("Inside first method: \n");
+
+        System.out.print(memberVariable + "\n");
+        System.out.println(localVariable);
+    }
+    private void anotherMethod() {
+        System.out.println("Inside second method: ");
+        System.out.println(memberVariable);
+    }
+
+    private void methodForLoopVariables() {
+
+        String localVariable = " Just another member variable outside our do while loop. ";
+
+        System.out.println(localVariable);
+
+        boolean runDoLoop = true;
+        String loopVariable = "I am a loop variable inside our do while loop";
+        do {
+            System.out.println(localVariable);
+            runDoLoop = false;
+        }while(runDoLoop);
+
+        System.out.println("Inside third method, just outside do while loop: ");
+        System.out.println(localVariable);
+        System.out.println(loopVariable);
+    }
+    private void memberVsLocalVariable{
+        int LocalInt = 7;
+
+        LocalInt *= 2;
+        memberInt *= 2;
+    }
+
+    private void getMemberVsLocalVariables (boolean runDoLoop, String name){
+        int localInt = 10;
+        System.out.println(runDoLoop);
+        System.out.println(name);
+        System.out.println(localInt);
+    }
+
 }
